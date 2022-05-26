aea39178bdb8109a799f6e569418a8cff1e5a4cb
ssmorgensen <96442073+ssmorgensen@users.noreply.github.com>
2022-05-25 20:43:48 -0700

    created and completed canDrive.js

---
+++ lesson02/canDrive.js
@@ -0,0 +1,13 @@
+let firstName = 'Elizabeth';
+let lastName = 'Lemon';
+let x = 'can drive';
+let currentAge = 42;
+const drivingAge = 16;
+let drivingStatus = firstName + ' ' + lastName + ' ' +  x + ' ' + (currentAge - drivingAge)
+for (let y = 5; y >= 0; y--)
+
+if (currentAge - drivingAge >=0) {
+  console.log (drivingStatus);
+} else {
+  console.log (y);
+}
