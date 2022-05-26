7f066e350b7eb6dda717a2998e35891f88320501
ssmorgensen <96442073+ssmorgensen@users.noreply.github.com>
2022-05-25 22:02:26 -0700

    created and completed cardDeck.js

---
+++ lesson02/cardDeck.js
@@ -0,0 +1,18 @@
+let suitOne = 'of hearts';
+let suitTwo = 'of clubs';
+let suitThree = 'of diamonds';
+let suitFour = 'of spades';
+
+
+for (let x = 13; x>0; x--) {
+  console.log ((x) + ' ' + suitOne)
+}
+for (let x = 13; x>0; x--) {
+  console.log ((x) + ' ' + suitTwo)
+}
+for (let x = 13; x>0; x--) {
+  console.log ((x) + ' ' + suitThree)
+}
+for (let x = 13; x>0; x--) {
+  console.log ((x) + ' ' + suitFour)
+}
